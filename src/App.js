5ed988adde1056fcdbbb7d8c054833c4344d0347
daehyunkim <find.acho@gmail.com>
2022-09-03 23:19:11 +0900

    style: create Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-
-import List from './pages/list';
-import Form from './pages/form'
-import Detail from './pages/detail';
-import Login from './pages/login';
-import Signup from './pages/signup';
-
-
-
+import Router from './shared/Router';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/api/postlist' element={<List/>} />
-        <Route path='/api/postlist/create' element={<Form/>} />
-        <Route path='/api/postlist:postId' element={<Detail/>} />
-        <Route path='/auth/login' element={<Login/>} />
-        <Route path='/auth/signup' element={<Signup/>} />
-      </Routes>
-    </div>
+    <Router/>
   );
 };
 
---
+++ src/shared/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import List from '../pages/list';
+import Form from '../pages/form'
+import Detail from '../pages/detail';
+import Login from '../pages/login';
+import Signup from '../pages/signup';
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/api/postlist' element={<List />} />
+        <Route path='/api/postlist/create' element={<Form />} />
+        <Route path='/api/postlist:postId' element={<Detail />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/auth/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default Router;
